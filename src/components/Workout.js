8b3523f3e67c94ce70a1faa4df668bcf8128636c
bex-ford <becky782@hotmail.co.uk>
2023-03-16 19:47:18 +0000

    basic layout for npm fitness-calc in Workout.js

--- src/components/Workout.js
+++ src/components/Workout.js
@@ -2,7 +2,33 @@ import React from "react";
 
 function Workout() {
   return (
-    <div class="container">
+    <div className="card">
+      <h2>Weights and Reps Calculator</h2>
+      <p>
+        Enter the weight and reps to get the estimate of your 1RM (one rep max)
+      </p>
+      <form>
+        <label>
+          Weight
+          <input type="text" name="name" />
+        </label>
+        <br></br>
+        <label>
+          Reps
+          <input type="text" name="name" />
+        </label>
+        <br></br>
+        <label>
+          Exercise
+          <select>
+            <option value="bench">Bench Press</option>
+            <option value="deadlift">Deadlift</option>
+            <option value="squat">Squat</option>
+          </select>
+        </label>
+        <br></br>
+        <input type="submit" value="Show Graph" />
+      </form>
     </div>
   );
 }
