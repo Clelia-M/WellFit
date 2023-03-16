96288bf54e307aa18c15217a9363450829065f4a
bex-ford <becky782@hotmail.co.uk>
2023-03-16 20:27:27 +0000

    HTML structure added to fitness-calc

--- src/components/Workout.js
+++ src/components/Workout.js
@@ -3,24 +3,28 @@ import React from "react";
 function Workout() {
   return (
     <div className="card">
-      <h2>Weights and Reps Calculator</h2>
+      <h2>Weight and Reps Calculator</h2>
       <p>
         Enter the weight and reps to get the estimate of your 1RM (one rep max)
       </p>
       <form>
         <label>
           Weight
-          <input type="text" name="name" />
+          <input type="text" weight="weight" />
+          <select name="liftmassunit" id="liftmassunit">
+            <option value="kg"> kg </option>
+            <option value="lb"> lb </option>
+            </select>
         </label>
         <br></br>
         <label>
           Reps
-          <input type="text" name="name" />
+          <input type="text" reps="reps" />
         </label>
         <br></br>
         <label>
           Exercise
-          <select>
+          <select> 
             <option value="bench">Bench Press</option>
             <option value="deadlift">Deadlift</option>
             <option value="squat">Squat</option>
@@ -29,6 +33,9 @@ function Workout() {
         <br></br>
         <input type="submit" value="Show Graph" />
       </form>
+      <br></br>
+      <h5>What is 1RM?</h5>
+      <p>One repo max ................</p>
     </div>
   );
 }
