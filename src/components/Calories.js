7150bb71f6bada5ff10dafff27cbc91f2ac098e9
bex-ford <becky782@hotmail.co.uk>
2023-03-17 19:11:08 +0000

    added buttons

--- src/components/Calories.js
+++ src/components/Calories.js
@@ -8,13 +8,13 @@ function Calories() {
         <div className="row">
           <div className="card text-center">
             <h2>Calories Calculator</h2>
-            <h5>See how many calories you have burned during your exercise..</h5>
+            <h5>
+              See how many calories you have burned during your exercise..
+            </h5>
             <form>
               <label>
                 Activity
-                <input
-                  value="activity"
-                />
+                <input value="activity" />
               </label>
               <br></br>
               <label>
@@ -24,9 +24,27 @@ function Calories() {
               <br></br>
               <label>
                 Duration
-                <input value="Duration" />
+                <input value="duration" />
               </label>
-              </form>
+              <br></br>
+               <button
+                className="btn btn-secondary btn-lg btn-block"
+                type="submit"
+              >
+                Submit
+              </button>
+              <br></br>
+              <button
+                className="btn btn-secondary btn-lg btn-block"
+                type="submit"
+              >
+                Reset
+              </button>
+            </form>
+            <div className="center">
+              <h5>Total amount of calories burned: </h5>
+            </div>
+            <br></br>
           </div>
         </div>
       </div>
