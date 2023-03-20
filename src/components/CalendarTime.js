f70cd71e1659537f3eb952d4fd146592faff7bb5
Jasmine <dr.jingyuezhao@outlook.com>
2023-03-20 22:06:58 +0000

    update Nutritions component with calendar and local storage added, minor changes made to CalendarTime

--- src/components/CalendarTime.js
+++ src/components/CalendarTime.js
@@ -1,16 +1,31 @@
-import React, { useState } from "react";
+import React, { forwardRef, useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
-
 function CalendarTime() {
-  const [date, setDate] = useState(new Date());
+  const [date, setDate] = useState();
+  const ExampleCustomInput = forwardRef(({ value, onClick, onChange }, ref) => (
+    <input
+      name="dateTime"
+      value={value}
+      className="example-custom-input"
+      onClick={onClick}
+      onChange={onChange}
+      ref={ref}
+      id="dateInput"
+    ></input>
+  ));
+
   return (
     <DatePicker
       showTimeInput
       dateFormat="MM/dd/yyyy h:mmaa"
       selected={date}
       onChange={date => setDate(date)}
+      customInput={<ExampleCustomInput />}
+      dayClassName={() => "example-datepicker-day-class"}
+      popperClassName="example-datepicker-class"
+      todayButton="TODAY"
     />
   );
 };
--- src/components/Nutritions.js
+++ src/components/Nutritions.js
@@ -1,53 +1,124 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import API from "../utils/API";
 import CalendarTime from "./CalendarTime";
 
 function Nutritions() {
   const [search, setSearch] = useState("")
   const [results, setResults] = useState([])
+  const [formData, setFormData] = useState({ dateTime: "", food: "" });
 
-  const searchNutrition = query => {
+  const searchFood = query => {
     API.search(query)
-      .then(res => setResults(res.data))
+      .then(res => {
+        setResults(res.data);
+        console.log(res.data);
+      })
       .catch(err => console.log(err));
   };
 
   const handleInputChange = event => {
     setSearch(event.target.value);
+    const { name, value } = event.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
   };
 
   const handleFormSubmit = event => {
     event.preventDefault();
-    searchNutrition(search);
+    searchFood(search);
+    let savedSearch = JSON.parse(localStorage.getItem("searchedFood")) || [];
+    savedSearch.push({
+      date: document.getElementById("dateInput").value,
+      meal: document.getElementById("food-search").value,
+    });
+    localStorage.setItem("searchedFood", JSON.stringify(savedSearch));
+    setSearch("");
+    setFormData({
+      dateTime: "",
+      food: ""
+    });
+  };
+
+  const calorieSum = () => {
+    let sum = 0;
+    for (let i = 0; i < results.length; i++) {
+      sum += results[i].calories;
+    }
+    console.log(sum);
+    return sum;
   };
 
   return (
     <div className="container">
-      <form>
-        <div className="form-group">
-          <label htmlFor="search">Search:</label>
-          <input
-            onChange={handleInputChange}
-            value={search}
-            name="search"
-            type="text"
-            className="form-control"
-            placeholder="Search for food"
-            id="search"
-          />
-          <button onClick={handleFormSubmit} className="btn btn-warning mt-3">
-            Search
-          </button>
+      <div className="card-deck">
+        <div className="col">
+          <div className="row">
+            <div className="card text-center">
+              <h2>Daily Intake Calories Calculator</h2>
+              <h5>How to lose weight?</h5>
+              <p>
+                Keep your net calories below your calculated daily calorie needs.
+                You can do this by eating fewer calories or exercising more.
+                For the best results, do both!
+                <br />
+                You can use the Daily Intake Calories Calculator tool to help you determine the right calorie limit for your wellness goals.
+                Your daily calorie limit is your target caloric intake per day.
+              </p>
+              <h5>Please provide the details of your meals (date/time, food name and its quantity)</h5>
+              <div className="dateInput mb-4">
+                <label className="font-weight-bold">Date & Time</label>
+                <CalendarTime />
+              </div>
+              <form>
+                <div className="form-group">
+                  <textarea
+                    onChange={handleInputChange}
+                    value={search}
+                    name="food"
+                    type="text"
+                    className="form-control"
+                    placeholder="For example: 1 cup mashed potatoes and 2 tbsp gravy"
+                    id="food-search"
+                  />
+                </div>
+                <button onClick={handleFormSubmit} className="btn btn-warning my-3" type="submit">
+                  Search
+                </button>
+              </form>
+              <div className="result-list mb-4">
+                <h5>Your calories intake:</h5>
+                <p className="text-muted small">Only one food per row</p>
+                <table className="table table-hover">
+                  <thead className="thead-dark">
+                    <tr>
+                      <th scope="col">Food name</th>
+                      <th scope="col">Serving size (g)</th>
+                      <th scope="col">Calories (kcal)</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {results.map(result => (
+                      <tr>
+                        <td>{result.name}</td>
+                        <td>{result.serving_size_g}</td>
+                        <td>{result.calories}</td>
+                      </tr>
+                    ))}
+                    <tr>
+                      <td colspan="2">Total calories:</td>
+                      <td id="sumCal">{calorieSum()}</td>
+                    </tr>
+                  </tbody>
+                </table>
+
+              </div>
+            </div>
+          </div>
         </div>
-      </form>
-      <ul className="list-group">
-        {results.map(result => (
-          <li className="list-group-item">
-            Calories of {result.name}: {result.calories}
-          </li>
-        ))}
-      </ul>
-    </div>
+      </div>
+    </div >
   );
 
 }
