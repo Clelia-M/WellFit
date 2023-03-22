34315e978819701868fbb414983910c6b02b335d
Clelia-M <114869080+Clelia-M@users.noreply.github.com>
2023-03-22 08:58:21 +0000

    cards review and final style

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -4,7 +4,7 @@ function Cards() {
   return (
     <div id="cards-container">
     <div className="container">
-      <h2 className="type-large-fat xl:type-56-bol text-center lg:pt-56 xl:pt-64 mb-32 xl:mb-64">What is WellFit?</h2>
+      <h2 id="wellfit" className="type-large-fat xl:type-56-bol text-center lg:pt-56 xl:pt-64 mb-32 xl:mb-64">What is WellFit?</h2>
       <div className="row row-cols-1 row-cols-md-2 g-4 text-center my-64">
         <div className="col">
           <div className="card border-0">
@@ -20,7 +20,7 @@ function Cards() {
             <img src="../images/undraw_personal_training_0dqn.png" className="card-img-top" alt="" />
             <div className="card-body">
               <h3 className="card-title mt-24 md:mt-40 mb-8 md:mb-16 type-med-fat">Your Workout calculator</h3>
-              <p className="card-text mb-16 type-default leading-body">Exercising regularly is key to reaching your health and fitness goals, and tracking your workouts calories burned can help you stay motivated and see progress. Keep track of your progress over time. Whether you prefer running, weightlifting, or yoga, we've got you covered. Start checking your workouts today and take the first step towards a healthier you!</p>
+              <p className="card-text mb-16 type-default leading-body">Exercising regularly is key to reaching your health and fitness goals, and calculating your workouts calories can help you stay motivated. Whether you prefer running, weightlifting, or yoga, we've got you covered. Start checking your workouts today and take the first step towards a healthier you!</p>
             </div>
           </div>
         </div>
